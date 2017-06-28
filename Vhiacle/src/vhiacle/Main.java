426af1b33a2090ddb723c6e4ee154e3b800e7e8a
TS-EU\gematrai <gergoe.matrai@t-systems.com>
2017-06-28 16:57:00 +0200

    version 1.01  választható járművekkel módosítva az adatbekérés

--- Vhiacle/src/vhiacle/Main.java
+++ Vhiacle/src/vhiacle/Main.java
@@ -17,7 +17,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Give the type of the vehicle:");
+        System.out.println("Give the type of the vehicle (can be : car, van):");
         String vhiacleType = scanner.next();
 
         if(vhiacleType.trim().toLowerCase().equals("van")) {
